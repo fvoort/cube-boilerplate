675ba841c65a9a5c93d8c641640220ad4cbd5865
Flip van de Voort <flip@fvoort.com>
2025-11-24 11:54:56 +0100

    Added nested tokens

--- src/css-utils/tokens-to-tailwind.js
+++ src/css-utils/tokens-to-tailwind.js
@@ -2,16 +2,29 @@ const slugify = require('slugify');
 
 /**
  * Converts human readable tokens into tailwind config friendly ones
+ * Supports infinitely nested token structures
  *
- * @param {array} tokens {name: string, value: any}
+ * @param {array} tokens {name: string, value: any, items?: array}
+ * @param {string} prefix - Internal parameter for recursion
  * @return {object} {key, value}
  */
-const tokensToTailwind = tokens => {
+const tokensToTailwind = (tokens, prefix = '') => {
   const nameSlug = text => slugify(text, {lower: true});
   let response = {};
 
-  tokens.forEach(({name, value}) => {
-    response[nameSlug(name)] = value;
+  tokens.forEach(({name, value, items}) => {
+    // Build the key, adding prefix if we're in a nested structure
+    const key = prefix ? `${prefix}-${nameSlug(name)}` : nameSlug(name);
+
+    // If this token has nested items, recurse into them
+    if (items && Array.isArray(items)) {
+      // Recursively process nested items with the current key as prefix
+      Object.assign(response, tokensToTailwind(items, key));
+    }
+    // Otherwise, it's a leaf node with a value
+    else if (value !== undefined) {
+      response[key] = value;
+    }
   });
 
   return response;
